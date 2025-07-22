183d222c5409442134d3b84ecbf70d75104c70af
aleksandrgineyka <meinepostkam@gmail.com>
2025-07-23 01:31:44 +0300

    changed annotation SpringBootTest

--- src/test/java/ru/yandex/practicum/service/impl/CommentServiceImplTest.java
+++ src/test/java/ru/yandex/practicum/service/impl/CommentServiceImplTest.java
@@ -1,19 +1,18 @@
 package ru.yandex.practicum.service.impl;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-import ru.yandex.practicum.configuration.DataConfig;
-import ru.yandex.practicum.configuration.TestConfig;
-import ru.yandex.practicum.dao.CommentDao;
 import ru.yandex.practicum.dto.PostCreateDto;
 import ru.yandex.practicum.model.Comment;
 import ru.yandex.practicum.model.Post;
@@ -23,16 +22,14 @@ import ru.yandex.practicum.service.PostService;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-@ContextConfiguration(classes = {DataConfig.class, TestConfig.class})
-@ExtendWith(SpringExtension.class)
-@Transactional(readOnly = true)
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 class CommentServiceImplTest {
     @Autowired
     private CommentService commentService;
@@ -74,9 +71,7 @@ class CommentServiceImplTest {
     }
 
 
-
     @Test
-    @Transactional
     void create() {
         Long id = createPost();
         Post postWithComment = postService.getById(id);
@@ -92,7 +87,6 @@ class CommentServiceImplTest {
     }
 
     @Test
-    @Transactional
     void update() {
         Long post_id = createPost();
         Comment comment = commentService.getAll(post_id).get(0);
@@ -102,12 +96,6 @@ class CommentServiceImplTest {
         Comment updatedComment = commentService.getAll(post_id).get(0);
 
         assertThat(updatedComment.getText(), equalTo(newText));
-    }
-
-    @Test
-    @Transactional
-    void delete() {
-
     }
 
     private Long createPost() {
