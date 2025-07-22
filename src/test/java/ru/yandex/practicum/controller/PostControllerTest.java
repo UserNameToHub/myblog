82100e531282265dc37d55965bf9da09d3f75b8c
aleksandrgineyka <meinepostkam@gmail.com>
2025-07-23 01:30:35 +0300

    changed annotation WebMvcTest

--- src/test/java/ru/yandex/practicum/controller/PostControllerTest.java
+++ src/test/java/ru/yandex/practicum/controller/PostControllerTest.java
@@ -2,15 +2,11 @@ package ru.yandex.practicum.controller;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.yandex.practicum.config.DataConfig;
-import ru.yandex.practicum.configuration.TestConfig;
 import ru.yandex.practicum.model.Post;
 import ru.yandex.practicum.service.PostService;
 import ru.yandex.practicum.util.StrUtil;
@@ -23,29 +19,27 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {DataConfig.class, TestConfig.class})
-@WebMvcTest(controllers = PostController.class)
+@WebMvcTest(PostController.class)
 class PostControllerTest {
-    @MockitoBean
-    private PostService postService;
-    private static Post post;
-
     @Autowired
     private MockMvc mockMvc;
 
+    @MockitoBean
+    public  PostService postService;
+
+    private static Post post;
+
     @BeforeAll
     static void setUp() {
-         post = Post.builder()
-                 .id(12l)
-                 .text("some text")
-                 .title("some title")
-                 .tags(new ArrayList<>())
-                 .comments(new ArrayList<>())
-                 .likeComment(123l)
-                 .imagePath("url")
-                 .build();
+        post = Post.builder()
+                .id(12l)
+                .text("some text")
+                .title("some title")
+                .tags(new ArrayList<>())
+                .comments(new ArrayList<>())
+                .likeComment(123l)
+                .imagePath("url")
+                .build();
     }
 
     @Test
